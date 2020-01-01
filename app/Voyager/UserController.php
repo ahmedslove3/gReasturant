cb3d31f6c3096a6ef32827b179fe4356b5486945
Ahmed Aldaly <ahmedslove3@gmail.com>
2020-01-01 11:22:34 +0200

    inital commit for the project

---
+++ app/Voyager/UserController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Voyager;
+
+use TCG\Voyager\Http\Controllers\VoyagerUserController as BaseVoyagerUserController;
+
+class UserController extends BaseVoyagerUserController
+{
+    //
+}
---
+++ resources/views/about.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+	<title>G. Reasturant - About Page</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet" />
+	<link href="css/all.min.css" rel="stylesheet" />
+	<link href="css/templatemo-style.css" rel="stylesheet" />
+</head>
+<!--
+
+G. Reasturant
+
+https://templatemo.com/tm-539-simple-house
+
+-->
+
+<body>
+
+	<div class="container">
+		<!-- Top box -->
+		<!-- Logo & Site Name -->
+		<div class="placeholder">
+			<div class="parallax-window" data-parallax="scroll" data-image-src="img/simple-house-01.jpg">
+				<div class="tm-header">
+					<div class="row tm-header-inner">
+						<div class="col-md-6 col-12">
+							<img src="http://localhost:8001/admin/voyager-assets?path=images%2Flogo-icon-light.png" style="width:80px" alt="Logo" class="tm-site-logo" />
+							<div class="tm-site-text-box">
+								<h1 class="tm-site-title">G. Reasturant</h1>
+								<h6 class="tm-site-description">Happiness is a full belly</h6>
+							</div>
+						</div>
+						<nav class="col-md-6 col-12 tm-nav">
+							<ul class="tm-nav-ul">
+								<li class="tm-nav-li"><a href="/" class="tm-nav-link active">Home</a></li>
+								<li class="tm-nav-li"><a href="/about" class="tm-nav-link">About</a></li>
+								<li class="tm-nav-li"><a href="/contact" class="tm-nav-link">Contact</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<main>
+			<header class="row tm-welcome-section">
+				<h2 class="col-12 text-center tm-section-title">About Our Restaurant</h2>
+				<p class="col-12 text-center">This a very good place come eat our food.</p>
+			</header>
+
+
+		</main>
+
+		<footer class="tm-footer text-center">
+			<p>Copyright &copy; 2020 G. Reasturant</p>
+		</footer>
+	</div>
+	<script src="js/jquery.min.js"></script>
+	<script src="js/parallax.min.js"></script>
+</body>
+
+</html>
